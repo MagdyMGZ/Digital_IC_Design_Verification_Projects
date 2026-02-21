25d9c90400eb5a317cfc0fb0ed14f25bb7725a23
Magdy Ahmed <91482114+MagdyMGZ@users.noreply.github.com>
2026-02-21 17:02:50 +0200

    Delete 05-RISCV_Single_Cycle directory

--- 05-RISCV_Single_Cycle/RTL_TB/Adder.v
+++
@@ -1,9 +0,0 @@
-module Adder (
-    input       wire        [31:0]      In1,
-    input       wire        [31:0]      In2,
-    output      wire        [31:0]      Out
-);
-
-assign Out = In1 + In2;
-
-endmodule
