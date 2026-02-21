121fb1e8885abc5b4eb06b8b9d292471b380bca3
Magdy Ahmed <91482114+MagdyMGZ@users.noreply.github.com>
2026-02-21 17:04:08 +0200

    Delete 12-ALSU directory

--- 12-ALSU/04-shift_reg_sequencer.svh
+++
@@ -1,8 +0,0 @@
-class shift_reg_sequencer extends uvm_sequencer #(shift_reg_seq_item);
- 
- `uvm_component_utils(shift_reg_sequencer);
-
- function new(string name="shift_reg_sequencer",uvm_component parent=null);
-     super.new(name,parent);
- endfunction
-endclass
