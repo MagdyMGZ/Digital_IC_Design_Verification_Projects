7bffcaf6f491392e057302c5cda6e0b36a43029f
Magdy Ahmed <91482114+MagdyMGZ@users.noreply.github.com>
2026-02-21 17:02:42 +0200

    Delete 04-AES_Encryption directory

--- 04-AES_Encryption/UVM/AES_sequencer.svh
+++
@@ -1,9 +0,0 @@
-class AES_sequencer extends uvm_sequencer #(AES_sequence_item);
-
-`uvm_component_utils(AES_sequencer)
-
-function new (string name = "AES_sequencer", uvm_component parent = null);
-    super.new(name,parent);
-endfunction
-
-endclass
