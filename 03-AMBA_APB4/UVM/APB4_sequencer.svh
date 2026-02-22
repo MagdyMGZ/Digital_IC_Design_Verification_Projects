a2d9c9a0632a2fec99e424e6f6dfb655774b384e
Magdy Ahmed <91482114+MagdyMGZ@users.noreply.github.com>
2026-02-22 13:56:29 +0200

    Delete 03-AMBA_APB4/UVM directory

--- 03-AMBA_APB4/UVM/APB4_sequencer.svh
+++
@@ -1,9 +0,0 @@
-class APB4_sequencer extends uvm_sequencer #(APB4_sequence_item);
-
-`uvm_component_utils(APB4_sequencer)
-
-function new (string name = "APB4_sequencer", uvm_component parent = null);
-    super.new(name,parent);
-endfunction
-
-endclass
