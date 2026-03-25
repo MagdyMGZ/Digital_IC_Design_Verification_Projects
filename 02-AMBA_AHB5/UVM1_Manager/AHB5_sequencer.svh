8ef1e050ddb573a8ac3cf88b3dccfe0c430410a9
Magdy Ahmed <91482114+MagdyMGZ@users.noreply.github.com>
2026-03-25 18:32:00 +0200

    Delete 02-AMBA_AHB5 directory

--- 02-AMBA_AHB5/UVM1_Manager/AHB5_sequencer.svh
+++
@@ -1,9 +0,0 @@
-class AHB5_sequencer extends uvm_sequencer #(AHB5_sequence_item);
-
-`uvm_component_utils(AHB5_sequencer)
-
-function new (string name = "AHB5_sequencer", uvm_component parent = null);
-    super.new(name,parent);
-endfunction
-
-endclass
